783a7b3f61b1f8a369ce2160baded9f3373b9240
zhouyb1024 <61892988+zhouyb1024@users.noreply.github.com>
2020-08-05 10:16:59 +0800

    Add files via upload

---
+++ Entry.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Entry {
+    public static void main(String[] args) {
+        System.out.println("hello java!");
+    }
+
+}
