8d424ecabf60c615982eed97eaef9df36379902c
Yangbo.Zhou <zhouyb1024@163.com>
2022-07-17 10:44:08 +0800

    so long not see, java

--- Entry.java
+++ Entry.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Entry {
     public static void main(String[] args) {
+		List<Integer> list = new ArrayList<>();
         System.out.println("hello java!");
     }
 
